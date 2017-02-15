cfda52d1c0c714fd5a3a48a9cebc2a319f345476
Edgar Aroutiounian <edgar.factorial@gmail.com>
2017-02-15 03:22:03 -0800

    Add GA

--- backend/server.js
+++ backend/server.js
@@ -49,6 +49,15 @@ const site = (tech_events, e_count) => `
     window.__ALL_TECH_EVENTS__ = ${JSON.stringify(tech_events)}
     window.__EVENT_COUNT_THIS_MONTH__ = ${e_count}
   </script>
+  <script>
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-91993227-1', 'auto');
+    ga('send', 'pageview');
+  </script>
 </head>
 <body>
   <div id='container'>${rendered}</div>
