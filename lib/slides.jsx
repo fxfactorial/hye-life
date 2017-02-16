da68eaa6b073501e004c46e45801562c3ea738f4
Edgar Aroutiounian <edgar.factorial@gmail.com>
2017-02-15 19:41:34 -0800

    Rid the evening pic

--- lib/slides.jsx
+++ lib/slides.jsx
@@ -1,7 +1,6 @@
 export default [
   {descr: 'Komitas in thought', src: 'komitas.jpg'},
   {descr: 'The Matendaran manuscript library', src: '1280px-Matenadaran_3.jpg'},
-  {descr: 'Evening in Yerevan', src: 'evening.jpg'},
   {descr: 'Manuscripts', src: 'Armenian_Manuscript_no._4_Wellcome_L0022854.jpg'},
   {descr: 'Armenian Yarkhuskta Dance', src: 'Armenian_martial_dance_Yarkhushta.jpg'},
   {descr: '18th Century Armenian Linen weave', src: 'Cover,_Armenia,_18th_century,_Linen,_silk,_plain_weave,_embroidery_(cross_stitch),_drawnwork_lace,_Honolulu_Academy_of_Arts.jpg'},
